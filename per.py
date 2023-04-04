5f5af7283bd52ad22dc3637ce223e15b1f688fad
supchik1604 <128502787+supchik1604@users.noreply.github.com>
2023-04-04 19:18:29 +0300

    ничего хорошего
    вы офигеете от этого (не обязательно, в хорошем смысле)

---
+++ per.py
@@ -0,0 +1,13 @@
+def abs (c):
+    while c > 0:
+        print(f - 1 + c)
+    print(c - 1)
+else c == 0:
+print("Все")
+a = int(input("Сколько компьютеров вы хотите подключить"))
+b = int(input("Сколько у вас разветвлителей с двумя разъемами"))
+c = int(input("Сколько у вас разветвлителей с тремя разъемами"))
+while b > 0:
+    f = print(1+b-1) and b - 1
+    if b == 0:
+        return(abs)
